7c478ab512371128b19220318b30a2bbe130f9b8
jaay13 <contact@jason-koch.com>
2026-05-30 15:44:47 +0200

    fix: include necessary headers

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,20 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 11:50:52 by jakoch            #+#    #+#             */
-/*   Updated: 2026/05/30 11:52:53 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/05/30 15:44:24 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#include <fcntl.h> // open
+#include <unistd.h> // read, close
+#include <stdio.h> // printf
 
-
-
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 
 char *get_next_line(int fd);
 
