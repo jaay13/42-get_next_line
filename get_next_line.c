89cf6e41852bf98e2f7d31f421305f3ac235c069
jaay13 <contact@jason-koch.com>
2026-05-30 15:43:55 +0200

    feat: first implementation of gnl and first helper function

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,63 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 11:50:21 by jakoch            #+#    #+#             */
-/*   Updated: 2026/05/30 11:54:15 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/05/30 15:41:35 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static char *read_next_line(char *stash, int fd);
+static char *append_buf(char *stash, char *buf);
+
 char	*get_next_line(int fd)
 {
+	static char *stash;
+	char *line;
+
+	if (fd < 0 || read(fd, NULL, 0) < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!stash)
+		stash = ft_calloc(1);
+	if (!ft_strchr(stash, '\n'))
+		stash = read_next_line(stash, fd);
+	if (!stash)
+		return (free(stash), NULL);
+	line = get_output_from(stash);
+	stash = clean_written_from(stash);
+	return (line);
+}
+
+static char *read_next_line(char *stash, int fd)
+{
+ char *buf;
+ int  bytes_read;
+
+ if (!stash)
+	stash = ft_calloc(1, 1);
+ buf = ft_calloc(BUFFER_SIZE + 1);
+ if (!buf)
+  return (NULL);
+ bytes_read = 1;
+ while (bytes_read > 0)
+ {
+  bytes_read = read(fd, buf, BUFFER_SIZE);
+  if (bytes_read == -1)
+   return (free(buf), NULL);
+  buf[bytes_read] = '\0';
+  stash = append_buf(stash, buf);
+  if (ft_strchr(stash, '\n'))
+   break ;
+ }
+ free (buf);
+ return (stash);
+}
+
+static char *append_buf(char *stash, char *buf)
+{
+ char *new_stash;
+
+ new_stash = ft_strjoin(stash, buf);
+ free(stash);
+ return (new_stash);
 }
