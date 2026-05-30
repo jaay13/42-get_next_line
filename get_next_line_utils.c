191a198c9474ed2f546b0fdef6c465d1fa7011cc
jaay13 <contact@jason-koch.com>
2026-05-30 15:44:10 +0200

    feat: implement ft_strjoin and ft_strchr utility functions

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,42 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 11:50:49 by jakoch            #+#    #+#             */
-/*   Updated: 2026/05/30 11:50:50 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/05/30 15:21:58 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len;
+
+	if (!s1 && !s2)
+		return (ft_strdup(""));
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = malloc(len);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1, len);
+	ft_strlcat(str, s2, len);
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
 
