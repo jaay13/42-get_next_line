93294d34fcc9be5cff5c023fd4da5f76d04e8f8a
jaay13 <contact@jason-koch.com>
2026-05-31 17:43:11 +0200

    refactor: update header file with new utility function declarations

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 11:50:52 by jakoch            #+#    #+#             */
-/*   Updated: 2026/05/30 15:44:24 by jakoch           ###   ########.fr       */
+/*   Updated: 2026/05/31 16:54:26 by jakoch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 #include <fcntl.h> // open
 #include <unistd.h> // read, close
 #include <stdio.h> // printf
+#include <stdint.h> // SIZE_MAX
+#include <stdlib.h> // malloc, free
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 5
@@ -23,5 +25,10 @@
 
 char *get_next_line(int fd);
 
-
+/* ---------------	UTILS	--------------- */
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 #endif
