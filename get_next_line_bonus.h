e47117674d5ed1b4454ae8b92f65a275852f622e
jaay13 <contact@jason-koch.com>
2026-06-01 13:35:00 +0200

    feat: add get_next_line_bonus header file

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jakoch <jakoch@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/30 11:50:52 by jakoch            #+#    #+#             */
+/*   Updated: 2026/06/01 13:26:26 by jakoch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+#include <fcntl.h> // open
+#include <unistd.h> // read, close
+#include <stdio.h> // printf
+#include <stdint.h> // SIZE_MAX
+#include <stdlib.h> // malloc, free
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char *get_next_line(int fd);
+
+/* ---------------	UTILS	--------------- */
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+#endif
